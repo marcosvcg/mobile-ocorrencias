7379748de76c44d71639d7b30dfa6ed00d5a0ca1
marcosvcg <marcos.vdcg@gmail.com>
2025-09-16 19:24:39 -0300

    FEAT: complementa VisualizarOcorrenciaModal

--- src/pages/ocorrencia/components/modals/VisualizarOcorrencia/VisualizarOcorrenciaModal.tsx
+++ src/pages/ocorrencia/components/modals/VisualizarOcorrencia/VisualizarOcorrenciaModal.tsx
@@ -1,9 +1,10 @@
 import type { Ocorrencia } from "../../../../../models/Ocorrencia";
 import { fetchObterConteudoDocumentoPeloID } from "../../../../../service/apiBackend";
 import { useEffect, useRef, useState } from "react";
-import { Editor } from "@tinymce/tinymce-react";
 import DOMPurify from "dompurify";
+import ImprimirPDF from "../../../../../util/ImprimirPDF";
 import "./css/VisualizarOcorrenciaModal.css";
+import PrinterIcon from "../../icons/PrinterIcon";
 
 interface Props {
   ocorrencia: Ocorrencia;
@@ -11,51 +12,36 @@ interface Props {
 }
 
 const VisualizarOcorrenciaModal = ({ onClose, ocorrencia }: Props) => {
-    const [documentoSolicitacao, setDocumentoSolicitacao] = useState("");
-    const editorRef = useRef<any>(null);
+  const [documentoSolicitacao, setDocumentoSolicitacao] = useState("");
+  const editorRef = useRef<any>(null);
 
-    useEffect(() => {
-        const carregarDocumentoSolicitacao = async () => {
-            const data = await fetchObterConteudoDocumentoPeloID(ocorrencia.documentos_solicitacao[0].id);
-            if(data) setDocumentoSolicitacao(DOMPurify.sanitize(data[0]));
-        };
-        carregarDocumentoSolicitacao();
-    }, []);
+  useEffect(() => {
+    const carregarDocumentoSolicitacao = async () => {
+      const data = await fetchObterConteudoDocumentoPeloID(ocorrencia.documentos_solicitacao[0].id);
+      if (data) setDocumentoSolicitacao(DOMPurify.sanitize(data[0]));
+    };
+    carregarDocumentoSolicitacao();
+  }, []);
 
-    const handlePrint = () => {
+  const handlePrint = () => {
     if (editorRef.current) {
       editorRef.current.execCommand("mcePrint");
-        }
-    };
+    }
+  };
 
   return (
     <div className="visualizar-ocorrencia-modal-container">
       <div className="visualizar-ocorrencia-modal">
         <div className="visualizar-ocorrencia-modal-header">
+          <div onClick={handlePrint}><PrinterIcon /></div>
           <span className="visualizar-ocorrencia-titulo">Detalhes da Ocorrência</span>
           <p className="visualizar-ocorrencia-close" onClick={() => onClose()}>&times;</p>
         </div>
-          <div className="visualizar-ocorrencia-modal-content">
-            <div className="visualizar-ocorrencia-documento" dangerouslySetInnerHTML={{ __html: documentoSolicitacao }} onClick={handlePrint}/>
+        <div className="visualizar-ocorrencia-modal-content">
+          <div className="visualizar-ocorrencia-documento" dangerouslySetInnerHTML={{ __html: documentoSolicitacao }} onClick={handlePrint} />
         </div>
       </div>
-
-      <div style={{ display: "none" }}>
-          <Editor
-          tinymceScriptSrc={"/tinymce/tinymce.min.js"}
-            onInit={(_, editor) => (editorRef.current = editor)}
-            value={documentoSolicitacao}
-            init={{
-              menubar: false,
-              toolbar: false,
-              plugins: "print",
-              readOnly: true,
-              branding: false,
-              statusbar: false,
-              height: 0,
-            }}
-          />
-        </div>
+      <ImprimirPDF editorRef={editorRef} documentoSolicitacao={documentoSolicitacao} />
     </div>
   );
 };
---
+++ src/util/ImprimirPDF.tsx
@@ -0,0 +1,30 @@
+import { Editor } from "@tinymce/tinymce-react";
+import "./ImprimirPDF.css";
+
+interface Props {
+    editorRef: React.RefObject<any>;
+    documentoSolicitacao: string;
+}
+
+const ImprimirPDF = ({ editorRef, documentoSolicitacao }: Props) => {
+    return (
+        <div className="imprimir-pdf">
+            <Editor
+                tinymceScriptSrc={"/tinymce/tinymce.min.js"}
+                onInit={(_, editor) => (editorRef.current = editor)}
+                value={documentoSolicitacao}
+                init={{
+                    menubar: false,
+                    toolbar: false,
+                    plugins: "print",
+                    readOnly: true,
+                    branding: false,
+                    statusbar: false,
+                    height: 0,
+                }}
+            />
+        </div>
+    );
+};
+
+export default ImprimirPDF
