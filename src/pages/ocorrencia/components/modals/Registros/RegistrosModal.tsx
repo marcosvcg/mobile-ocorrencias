e583b3623fd724fa2350a2b13f1c950c39003756
marcosvcg <marcos.vdcg@gmail.com>
2025-09-24 17:48:05 -0300

    REFACTOR: desabilita envio de anexos p solicitações concluídas

--- src/pages/ocorrencia/components/modals/Registros/RegistrosModal.tsx
+++ src/pages/ocorrencia/components/modals/Registros/RegistrosModal.tsx
@@ -14,46 +14,48 @@ const RegistrosModal = ({ ocorrencia, onClose, onEnviarFotos }: Props) => {
 
   return (
     <>
-    <div className="registros-modal-container">
-      <div className="registros-modal">
-        <div className="registros-modal-header">
-          <span className="registros-titulo">Registros Internos</span>
-          <p className="registros-close" onClick={() => onClose()}>
-            &times;
-          </p>
-        </div>
-        <Swiper
-          modules={[Pagination]}
-          grabCursor
-          initialSlide={0}
-          centeredSlides
-          slidesPerView="auto"
-          speed={500}
-          slideToClickedSlide
-          autoHeight={true}
-          pagination={{ clickable: true }}
-        >
-          <div className="registros-modal-content">
-            {ocorrencia.anexos_internos.length === 0 
+      <div className="registros-modal-container">
+        <div className="registros-modal">
+          <div className="registros-modal-header">
+            <span className="registros-titulo">Registros Internos</span>
+            <p className="registros-close" onClick={() => onClose()}>
+              &times;
+            </p>
+          </div>
+          <Swiper
+            modules={[Pagination]}
+            grabCursor
+            initialSlide={0}
+            centeredSlides
+            slidesPerView="auto"
+            speed={500}
+            slideToClickedSlide
+            autoHeight={true}
+            pagination={{ clickable: true }}
+          >
+            <div className="registros-modal-content">
+              {ocorrencia.anexos_internos.length === 0
                 ? <span className="registros-nenhum-registro">Nenhum registro enviado.</span>
                 : ocorrencia.anexos_internos.map((registro, index) => (
-                <>
-                <SwiperSlide key={index}>
-                    <img className="registros-fotos" src={registro.arquivo} alt={registro.assunto} />
-                </SwiperSlide>
-                <div className="swiper-pagination"></div>
-              </>
-            ))}
-          </div>
-        </Swiper>
-      </div>
-        <div className="registros-enviar-fotos-div"> 
-            <button className="registros-button-enviar-fotos" 
-                    onClick={onEnviarFotos}
-              >Enviar Fotos
+                  <>
+                    <SwiperSlide key={index}>
+                      <img className="registros-fotos" src={registro.arquivo} alt={registro.assunto} />
+                    </SwiperSlide>
+                    <div className="swiper-pagination"></div>
+                  </>
+                ))}
+            </div>
+          </Swiper>
+        </div>
+        <div className="registros-enviar-fotos-div">
+          {ocorrencia.status !== "Concluído" && (
+            <button className="registros-button-enviar-fotos"
+              onClick={onEnviarFotos}
+            >Enviar Fotos
             </button>
-         </div>
-    </div>
+          )}
+        </div>
+      </div>
     </>
   );
 };
